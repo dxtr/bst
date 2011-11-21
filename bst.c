5ccd69dfd24fbc2763feebad74d0218d0c0f2432
Kim Lidström <kim.lidas+github@gmail.com>
2011-11-21 05:42:05 +0100

    Fixed memory leaks among other stuff.

--- bst.c
+++ bst.c
@@ -1,75 +1,160 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <list/list.h>
 #include "bst.h"
 
-bst_node *bst_new_node(void *data)
+/* Creates a new node
+ * Arguments: the data the node will contain or NULL for an empty node
+ */
+bst_node *bst_new_node(uint64_t key, void *data)
 {
 	struct bst_node *new_node = malloc(sizeof(struct bst_node));
-	if (data == NULL || new_node == NULL) goto fail;
+	if (new_node == NULL) return NULL;
+	new_node->list = list_create(data);
+	if (new_node->list == NULL) {
+		free(new_node);
+		new_node = NULL;
+		return NULL;
+	}
 
-	new_node->data = data;
+	new_node->key = key;
 	new_node->left = NULL;
 	new_node->right = NULL;
 
-ret:
 	return new_node;
-fail:
-	if (new_node != NULL) {
-		free(new_node);
-		new_node = NULL;
-	}
-	goto ret;
 }
 
-bst_node* bst_insert(bst_node *root, int (*compare)(void*,void*), void *data)
+/* Creates a new node and inserts it in the tree specified by root
+ * Arguments: the tree, a comparison function and the data the node will contain
+ * If NULL is passed instead of the comparison function it will blindly insert
+ * the data (Not taking duplicates into consideration, etc)
+ */
+bst_node* bst_insert(bst_node** root, uint64_t key, int (*compare)(list_node*,void*), void *data)
 {
-	if (root == NULL || compare == NULL || data == NULL) return NULL;
+	if (root == NULL || data == NULL) return NULL; // We return NULL because we won't insert any data anyway
 
-	bst_node** node = bst_search(&root, compare, data);
-	if (*node == NULL) *node = bst_new_node(data);
+	bst_node** node = bst_search(root, key);
+	if (*node == NULL) {
+		*node = bst_new_node(key, data);
+	} else if (list_find((*node)->list, compare, data) == NULL) {
+		list_insert((*node)->list, data);
+	}
+
 	return *node;
 }
 
-bst_node** bst_search(bst_node **root, int (*compare)(void*,void*), void *data)
+/* Inserts an already created node in a tree
+ * Arguments: The tree to insert to and the node to insert
+ */
+bst_node *bst_insert_node(bst_node** root, bst_node* node)
 {
-	if (root == NULL || compare == NULL || data == NULL) return NULL;
+	if (root == NULL || *root == NULL || node == NULL) return NULL;
+
+	bst_node** s_node = bst_search(root, node->key);
+	if (*s_node == NULL) {
+		*s_node = node;
+		return *s_node;
+	} /*else if (list_merge((*s_node)->list, compare, node->list) > 0) {
+		return *s_node;
+	}*/
+
+	return NULL;
+}
+
+/* Searches for the data in the tree by using the comparison function
+ * Arguments: The tree, comparison function and the data to search for
+ */
+bst_node** bst_search(bst_node **root, uint64_t key)
+{
+	if (root == NULL || *root == NULL) return NULL;
 	struct bst_node** node = root;
 
 	while (*node != NULL) {
-		int result = compare(data, (*node)->data);
-		if (result < 0) node = &(*node)->left;
-		else if (result > 0) node = &(*node)->right;
+		if (((*node)->key) < key) node = &((*node)->left);
+		else if (((*node)->key) > key) node = &((*node)->right);
 		else break;
 	}
 
 	return node;
 }
 
+/* Delete a node
+ * Arguments: The node. Duh...
+ */
 void bst_delete(bst_node **node)
 {
 	if (node == NULL || *node == NULL) return;
 	bst_node *old_node = *node;
-	if ((*node)->left == NULL && ((*node)->right != NULL)) {
+	printf("node: %p, left: %p, right: %p\n", *node, (*node)->left, (*node)->right);
+	if ((*node)->left == NULL && (*node)->right != NULL) {
 		*node = (*node)->right;
 		bst_free_node(old_node);
-	} else if ((*node)->right == NULL && ((*node)->left != NULL)) {
+		old_node = NULL;
+	} else if ((*node)->right == NULL && (*node)->left != NULL) {
 		*node = (*node)->left;
 		bst_free_node(old_node);
-	} else if ((*node)->right == NULL && ((*node)->left == NULL)) {
+		old_node = NULL;
+	} else if ((*node)->right == NULL && (*node)->left == NULL) {
 		bst_free_node(*node);
 		*node = NULL;
 	} else {
-		bst_node **pred = &(*node)->left;
-		while ((*pred)->right != NULL) pred = &(*pred)->right;
+		bst_node **pred = &((*node)->left);
+		bst_node *tmp_node = NULL;
 
-		void *temp = (*pred)->data;
-		(*pred)->data = (*node)->data;
-		(*node)->data = temp;
+		while ((*pred)->right != NULL) pred = &((*pred)->right);
+		if ((*pred)->left != NULL) { 
+			printf("Left isn't NULL, there will be stray nodes..\n");
+			tmp_node = (*pred)->left;
+		}
+
+		list_node *temp = (*pred)->list;
+		(*pred)->list = (*node)->list;
+		(*node)->list = temp;
+		(*node)->key = (*pred)->key;
 		bst_free_node(*pred);
 		*pred = NULL;
+
+		if (tmp_node != NULL) {
+			printf("Re-inserting stray nodes...\n");
+			bst_insert_node(node, tmp_node);
+		}
 	}
 }
 
+/* DELETE ALL THE THINGS!
+ * Arguments: Quite obvious. The tree to destroy
+ */
+void bst_destroy(bst_node **node)
+{
+	if (node == NULL || *node == NULL) return;
+	while (*node) {
+		bst_delete(node);
+	}
+}
+
+/* Free a node.
+ * Arguments: ...
+ */
 void bst_free_node(bst_node *node)
 {
+	if (node == NULL) return; 
+	list_destroy(&(node->list));
+	memset(node, 0, sizeof(bst_node));
 	free(node);
+	node = NULL;
 }
+
+/* Function for creating a hash key from a string.
+ * Using djb found at http://www.cse.yorku.ca/~oz/hash.html
+ * Simply returns the hash
+ */ 
+uint64_t hash(const char *str)
+{
+	if (str == NULL) return 0;
+	uint64_t h = 5381;
+	for(; *str; str++) h = 33*h+(*str);
+	return h;
+}
+
--- bst.h
+++ bst.h
@@ -1,18 +1,26 @@
 #ifndef _BST_H
 #define _BST_H
 
+#include <stdint.h>
+#include <list/list.h>
+
 typedef struct bst_node {
-	void *data;
+	uint64_t key;
+	list_node *list;
 	struct bst_node *left;
 	struct bst_node *right;
 } bst_node;
 
-bst_node* bst_new_node(void *data);
-bst_node* bst_insert(bst_node *root, int (*compare)(void*,void*), void *data);
-bst_node** bst_search(bst_node **root, int (*compare)(void*,void*), void *data);
+bst_node* bst_new_node(uint64_t key, void *data);
+bst_node* bst_insert(bst_node **root, uint64_t key, int (*compare)(list_node*,void*), void *data);
+bst_node* bst_insert_node(bst_node **root, bst_node *node);
+bst_node** bst_search(bst_node **root, uint64_t key);
 void bst_delete(bst_node **node);
-
+void bst_destroy(bst_node **node);
 void bst_free_node(bst_node *node); 
+
+// hashing function
+uint64_t hash(const char *str);
 
 #endif
 
